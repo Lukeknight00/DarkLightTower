ca006ef865dab88aa55de73e6363cf8eefd72b17
Lukeknight00 <Lukeknight00@gmail.com>
2021-06-12 18:28:08 -0700

    create this biacth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+/// <reference path="../TSDef/p5.d.ts" />
 import './App.css';
+import React from 'react'
+import Sketch from 'react-p5';
+import p5Types from "p5";
+
+
+
 
 function App() {
+  let x1 =100;
+  let x2 =300;
+  let a;
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight).parent(canvasParentRef);
+    a = p5.createA("https://lukeknight.site","linkers");
+    p5.frameRate(30);
+  }
+  
+  const draw = p5 => {
+    p5.background(255, 130, 20);
+    p5.ellipse(x1, 100, 100);
+    a.position(x1,100);
+    p5.ellipse(x2, 100, 100);
+    x1 +=p5.sin(p5.frameCount/10);
+    x2 +=p5.sin(p5.frameCount/10);
+    
+   
+
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+          <Sketch setup={setup} draw={draw} />
     </div>
   );
 }
+
 
 export default App;
