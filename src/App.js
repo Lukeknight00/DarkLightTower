af2991c06f734d1f651ea1b200f7a8f101ca1253
Lukeknight00 <Lukeknight00@gmail.com>
2021-07-08 14:00:00 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   let orbSizeSlider, playerLevelSlider, qualitySlider, chargesSlider, sideSlider;
   let button;
   let qualitySliderValues = ["Common", "Uncommon", "Rare", "Very Rare", "Legendary"];
+  let sideTitleDiv;
 
   const preload = (p5) => {
 
@@ -36,7 +37,11 @@ function App() {
     button.class("btn third");
     button.position(p5.windowWidth / 2 - button.getWidth, p5.windowHeight - p5.windowHeight * .1);
 
-
+    //the title of the selected side
+    sideTitleDiv = p5.createDiv('Test');
+    sideTitleDiv.style('font-size', '42px');
+    sideTitleDiv.style('color','white');
+    sideTitleDiv.position(p5.windowWidth / 2 - button.getWidth, p5.windowHeight - p5.windowHeight * .15);
     //button.mousePressed(changeBG);
 
     //ORB SIZE SLIDER
@@ -81,10 +86,11 @@ function App() {
 
 
   const windowResized = (p5) => {
-    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    p5.resizeCanvas(p5.windowWidth, p5.windowHeight); 
 
     //update the layout
     button.position(p5.windowWidth / 2 - button.getWidth, p5.windowHeight - p5.windowHeight * .1);
+    sideTitleDiv.position(p5.windowWidth / 2 - button.getWidth, p5.windowHeight - p5.windowHeight * .15);
     sideSlider.position(p5.windowWidth / 4, 50);
     sideSlider.style('width', p5.windowWidth.toString() / 2 + 'px');
     sideSlider.style('height', p5.windowHeight.toString() / 2 + 'px')
@@ -104,7 +110,7 @@ function App() {
     //the light
     p5.ambientLight(12);
 
-    p5.lightFalloff(4 / (orbSizeSlider.value() * Math.abs((p5.sin(p5.millis() / 7500 - .5)))), 0, 0);
+    p5.lightFalloff(3 / (orbSizeSlider.value() * Math.abs(2+(p5.sin(p5.millis() / 3000 - 0)))), 0, 0);
     p5.pointLight(255, 255, 255, 0, -cylHeight / 2 - 100, 0);
 
 
@@ -149,7 +155,7 @@ function App() {
     p5.sphere(4000);
     //rock
     var sphereRadius = 10000;
-    p5.fill(30, 20, 0);
+    p5.fill(10, 10, 0);
     p5.translate(0, cylHeight / 2 + sphereRadius, 0);
     p5.sphere(sphereRadius);
     p5.pop();
@@ -162,17 +168,16 @@ function App() {
         rotation = 9 / 6 * p5.PI;
         //title
         p5.push();
-        p5.translate(0, 0, 200);
-        p5.textSize(100);
-        p5.fill(255);
-        p5.textAlign(p5.CENTER);
-        p5.text("Reduce", 0, p5.windowHeight / 2 - 2 * p5.textSize());
+        {
+          sideTitleDiv.html("Reduce");
+        }
         p5.pop();
 
         //the Button:
         p5.push();
         button.style('opacity', "100");
         button.html("Reduce your item to Precipitate");
+        button.mousePressed(reduce);
         p5.pop();
         break;
 
@@ -183,11 +188,7 @@ function App() {
         //title
         p5.push();
         {
-          p5.translate(0, 0, 200);
-          p5.textSize(100);
-          p5.fill(255);
-          p5.textAlign(p5.CENTER);
-          p5.text("Recharge", 0, p5.windowHeight / 2 - 2 * p5.textSize());
+          sideTitleDiv.html("Recharge");
         }
         p5.pop();
 
@@ -195,6 +196,8 @@ function App() {
         p5.push();
         button.style('opacity', "100");
         button.html("Recharge your item up to " + Math.floor(chargesSlider.value() * 1.5));
+        button.mousePressed(recharge);
+
        // orbSizeSlider.value(orbSizeSlider.value() - 2);
         p5.pop();
         break;
@@ -205,12 +208,11 @@ function App() {
         rotation = p5.PI / 6;
         //title
         p5.push();
-        p5.translate(0, 0, 200);
-        p5.textSize(100);
-        p5.fill(255);
-        p5.textAlign(p5.CENTER);
-        p5.text("Reforge", 0, p5.windowHeight / 2 - 2 * p5.textSize());
+        {
+          sideTitleDiv.html("Reforge");
+        }
         p5.pop();
+
 
         //the Button:
         p5.push();
@@ -219,7 +221,8 @@ function App() {
         let highQuality = qualitySliderValues[qualitySlider.value() + 1];
         lowQuality ?? (lowQuality = qualitySliderValues[0]);
         highQuality ?? (highQuality =qualitySliderValues[4]);
-        button.html("Reforge your item to an item between " + lowQuality + " and " +highQuality);
+        button.html("Reforge your item to an item between " + lowQuality + " and " + highQuality);
+        button.mousePressed(reforgePress);
         p5.pop();
         break;
       //side 3 : Reinfuser - change a magical artifact into something completly new of the same level or +-1 ()
@@ -228,17 +231,16 @@ function App() {
         rotation = -p5.PI / 6;
         //title
         p5.push();
-        p5.translate(0, 0, 200);
-        p5.textSize(100);
-        p5.fill(255);
-        p5.textAlign(p5.CENTER);
-        p5.text("Reinfuse", 0, p5.windowHeight / 2 - 2 * p5.textSize());
+        {
+          sideTitleDiv.html("Reinfuse");
+        }
         p5.pop();
 
         //the Button:
         p5.push();
         button.style('opacity', "100");
         button.html("Reduce your item to Precipitate");
+        button.mousePressed(reinfuse);
         p5.pop();
         break;
 
@@ -246,14 +248,19 @@ function App() {
         rotation = 1.3 * p5.sin(p5.millis() / 2500) + 3 * p5.PI / 12;
         //title
         p5.push();
-        p5.textSize((rotation + 1) * 10 + 10);
-        p5.fill(255);
-        p5.textAlign(p5.CENTER);
-        p5.text("Welcome to Dark Light Tower", 0, -p5.windowHeight / 2 + p5.textSize());
+        {
+          sideTitleDiv.html("Precipitate at " + orbSizeSlider.value()*10 + " percent");
+          p5.textSize((rotation + 1) * 10 + 10);
+          p5.fill(255);
+          p5.textAlign(p5.CENTER);
+          p5.text("Welcome to Dark Light Tower", 0, -p5.windowHeight / 2 + p5.textSize());
+        }
         p5.pop();
         //the Button:
         p5.push();
-        button.style('opacity', "0");
+          button.style('opacity', "0");        
+          button.mousePressed();
+
         p5.pop();
 
     }
@@ -327,7 +334,183 @@ function App() {
 
 
   const mouseClicked = p5 => {
-    //focus++;
+  //focus++;
+  }
+
+   //click handler
+  const reforgePress= p5 =>{
+    orbSizeSlider.value(orbSizeSlider.value()-qualitySlider.value());
+    reforge(qualitySlider.value());
+  }
+
+  const reduce = p5 => {
+  //the back : Destroy any object large enough to fit into it and add its essence to the tower(0 for common, 10% for uncommon/simple living creature, 
+  //10% for powerful non magical being,  20% for Rare/magic user, 30% for Very Rare / magical being, 60% legendary / god )
+  //if there is more than 100% then it goes into overflow, and creates a magic item in the reforger, preportional to the overflow.
+
+    const addPrecipitate = ( current,  adding) => {
+      if(current>=10){
+        alert("Your item disappears before you, the light on the top of the tower starts to glow, as an item appears in the reforger slot.");
+        reforge(adding);
+      }
+      else if(adding>=1){
+        adding--;
+        orbSizeSlider.value(orbSizeSlider.value()+1);
+        if (adding===0){
+          return;
+        }
+        addPrecipitate(orbSizeSlider.value(), adding);
+      }
+        return;
+    }
+
+
+    switch(qualitySlider.value()){
+      case 1:
+      //common
+        alert("Your item disappears before you, with no effect on the tower");
+        chargesSlider.value(0);
+       
+       break;
+      case 2:
+      //uncommon
+        addPrecipitate(orbSizeSlider.value(),1);
+        chargesSlider.value(0);
+        qualitySlider.value(1);
+        
+       break;
+      case 3:
+      //rare
+      addPrecipitate(orbSizeSlider.value(),2);
+        chargesSlider.value(0);
+        qualitySlider.value(1);
+       break;
+      case 4:
+      //very rare
+      addPrecipitate(orbSizeSlider.value(),4);
+        chargesSlider.value(0);
+        qualitySlider.value(1);
+       break;
+      case 5:
+      //legendary
+      addPrecipitate(orbSizeSlider.value(),6);
+        chargesSlider.value(0);
+        qualitySlider.value(1);
+       break;
+      default:
+      //it broke
+       break;
+    }
+
+    
+
+    
+
+  }
+  const recharge = p5 => {
+  //side 1 : Recharger - pull a small amount of energy to overcharge any aitem up to 1.5 times its maximum number of charges(rounding down) 
+    if (orbSizeSlider.value()>0){
+      orbSizeSlider.value(orbSizeSlider.value()-1);
+      alert("Your item is overcharged!")
+    }else{
+      alert("not enough precipitate");
+    }
+
+  }
+  const reforge = energy => {
+    
+    var roll;
+    energy = energy + Math.floor(playerLevelSlider.value()/8);
+  //side 2 : Reforger - Completly change the essence of the item: try and increase the rarity of the item overall by infusing it with the power of a random plane of existance, do nothing, or sap some of its strength and add it to the tower.
+  //energy and rarity are kinda the same thing
+    switch(energy){
+      case 1 :
+        roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table A");
+        break;
+      case 2 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table B");
+        break;
+      case 3 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table C");
+        break;
+      case 4 :
+      roll =Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table D");
+        break;
+      case 5 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table E");
+        break;
+      case 6 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table F");
+        break;
+      case 7 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table G");
+        break;
+      case 8 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table H");
+        break;
+      case 9 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table I");
+        break;
+      default :
+      alert("There is not enough precipitate used, and the reforge is wasted") 
+    }
+
+
+
+  }
+  const reinfuse = p5 => {
+    var roll;
+  //side 3 : Reinfuser - change a magical artifact into something completly new of the same level or +-1 ()
+  switch(qualitySlider.value() + Math.floor(playerLevelSlider.value()/5)){
+      case 1 :
+        roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table A");
+        break;
+      case 2 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table B");
+        break;
+      case 3 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table C");
+        break;
+      case 4 :
+      roll =Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table D");
+        break;
+      case 5 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table E");
+        break;
+      case 6 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table F");
+        break;
+      case 7 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table G");
+        break;
+      case 8 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table H");
+        break;
+      case 9 :
+      roll = Math.floor(Math.random()*100);
+        alert("You Forged a new item! You rolled a " + roll +" on magic item table I");
+        break;
+      default :
+      alert("There is not enough precipitate used, and the item is destroyed")
+
+  }
   }
 
 
